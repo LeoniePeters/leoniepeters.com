31da898386081ce7282391ab53a6ae37bdbba38e
LeoniePeters <leoniepeters@gmail.com>
2020-06-28 21:37:48 +0200

    Unset styles on main. no max-width. little cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.scss";
 import Welcome from "./Components/Welcome";
-import About from "./Components/About";
 import Portfolio from "./Components/Portfolio";
 
 function App() {
