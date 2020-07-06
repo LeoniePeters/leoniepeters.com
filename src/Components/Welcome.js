cace197eef548faf34cbe7448d6b85b3fb2cae64
LeoniePeters <leoniepeters@gmail.com>
2020-07-06 21:25:34 +0200

    Turn Welcome component into functional component. function toggleMode

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,34 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Welcome.scss";
 
-function Welcome() {
-  return (
-    <div className="Welcome">
-      <div className="whoami hexagon">
-        <h1>Leonie Peters: web developer</h1>
+class Welcome extends Component {
+  state = {
+    lightmode: false,
+  };
+
+  toggleMode = () => {
+    this.setState({ lightmode: !this.state.lightmode });
+    this.state.lightmode
+      ? document.body.classList.add("lightmode-on")
+      : document.body.classList.remove("lightmode-on");
+  };
+
+  render() {
+    return (
+      <div className="Welcome">
+        <div className="whoami hexagon">
+          <h1>Leonie Peters: web developer</h1>
+        </div>
+        <div className="hoi hexagon">
+          <span role="img" aria-label="zwaaiende hand">
+            👋🏻
+          </span>
+        </div>
+        <div className="mode-icons hexagon">
+          {!this.state.lightmode && (
+            <button type="button" onClick={this.toggleMode}>
+              <i class="icon-sun"></i>
+            </button>
+          )}
+          {this.state.lightmode && (
+            <button type="button" onClick={this.toggleMode}>
+              <i class="icon-moon"></i>
+            </button>
+          )}
+        </div>
+        <div className="history hexagon">
+          <p>
+            Art historian turned coder thanks to{" "}
+            <a href="https://codaisseur.com/">Codaisseur</a>.
+          </p>
+        </div>
+        <div className="job hexagon">
+          <p>
+            Currently front-end developer @{" "}
+            <a href="https://www.ontwerpwerk.nl/">Ontwerpwerk</a>.
+          </p>
+        </div>
+        <div className="stack hexagon">
+          <p>♥ Sass, JavaScript, React</p>
+        </div>
       </div>
-      <div className="hoi hexagon">
-        <span role="img" aria-label="zwaaiende hand">
-          👋🏻
-        </span>
-      </div>
-      <div className="history hexagon">
-        <p>
-          Art historian turned coder thanks to{" "}
-          <a href="https://codaisseur.com/">Codaisseur</a>.
-        </p>
-      </div>
-      <div className="job hexagon">
-        <p>
-          Currently front-end developer @{" "}
-          <a href="https://www.ontwerpwerk.nl/">Ontwerpwerk</a>.
-        </p>
-      </div>
-      <div className="stack hexagon">
-        <p>♥ Sass, JavaScript, React</p>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Welcome;
